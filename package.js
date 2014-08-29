7f8a286604ca8b8236a34d18c99f8e25aa00029d
David Yahalomi <davidyaha@gmail.com>
2014-08-29 21:51:58 +0300

    Bumped version

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 
 Package.describe({
   summary: "ng-grid - Angular Data Grid written in AngularJS and jQuery by the AngularUI Team",
-  version: "0.0.1",
+  version: "0.0.3",
   git: "https://github.com/davidyaha/meteor-ng-grid.git" 
 });
 
 Package.on_use(function (api) {
   api.versionsFrom('METEOR@0.9.0.1');
 
+  api.use('underscore', 'client');
   api.use('urigo:ngmeteor@0.2.0', 'client');
     
   // Exports the ngMeteorGrid options object
