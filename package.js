f6ae4f0403bba5f1b0aaa0f9a39e83b311ffe3c7
David Yahalomi <davidyaha@gmail.com>
2014-08-29 20:05:44 +0300

    prepared package.js for publishing

--- package.js
+++ package.js
@@ -1,32 +1,29 @@
+
 Package.describe({
-  summary: "ng-grid - Angular Data Grid written in AngularJS and jQuery by the AngularUI Team"
+  summary: "ng-grid - Angular Data Grid written in AngularJS and jQuery by the AngularUI Team",
+  version: "0.0.1",
+  git: "https://github.com/davidyaha/meteor-ng-grid.git" 
 });
 
-function packageExists(pkgname) {
-    var fs = Npm.require('fs');
-    var path = Npm.require('path');
-    var pkgpath = path.join('packages', pkgname);
-    return fs.existsSync(pkgpath);
-}
-
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.0.1');
 
-    api.use('urigo:ngmeteor@0.2.0', 'client');
+  api.use('urigo:ngmeteor@0.2.0', 'client');
     
-    // Exports the ngMeteorGrid options object
-    api.export('ngMeteorGrid', 'client');
+  // Exports the ngMeteorGrid options object
+  api.export('ngMeteorGrid', 'client');
 	
-    // Files to load in Client only.
-    api.add_files([
-		   'lib/ui-grid-unstable.js',
-		   'lib/ui-grid-unstable.css',
-	  	   'lib/ui-grid.eot',
-		   'lib/ui-grid.svg',
-		   'lib/ui-grid.ttf',
-		   'lib/ui-grid.woff',
-		   'smart.json', // Install bower components - ngGrid
-                   'options.js', // Options object declaretion
-                   'init.js'], // Addition of ngGrid to ngMeteor 
-                   'client');
+  // Files to load in Client only.
+  api.add_files([
+	   'lib/ui-grid-unstable.js',
+	   'lib/ui-grid-unstable.css',
+  	   'lib/ui-grid.eot',
+	   'lib/ui-grid.svg',
+	   'lib/ui-grid.ttf',
+	   'lib/ui-grid.woff',
+	   'smart.json', // Install bower components - ngGrid
+           'options.js', // Options object declaretion
+           'init.js'], // Addition of ngGrid to ngMeteor 
+           'client');
     
 });
